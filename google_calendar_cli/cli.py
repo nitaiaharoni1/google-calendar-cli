0f99eb023188bbf79f2a1eb709ee892bd42dfd9d
nitaiaharoni <nitaiaharoni1@gmail.com>
2026-01-15 12:19:31 +0200

    fixes version to 1.6.0

--- google_calendar_cli/cli.py
+++ google_calendar_cli/cli.py
@@ -19,7 +19,7 @@ from .history import add_operation, get_recent_operations, get_last_undoable_ope
 
 
 @click.group()
-@click.version_option(version="1.5.0")
+@click.version_option(version="1.6.0")
 @click.option("--account", "-a", help="Account name to use (default: current default account or GOOGLE_CALENDAR_ACCOUNT env var)")
 @click.option("--verbose", "-v", is_flag=True, help="Enable verbose/debug logging")
 @click.pass_context
